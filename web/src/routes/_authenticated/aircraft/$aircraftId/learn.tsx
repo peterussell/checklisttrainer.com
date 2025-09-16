7387efb8d3345d511b38e05dbe7e4dfc57a146bb
Pete Russell <pinkbatz@gmail.com>
2025-09-16 18:12:52 +1200

    Stub out learn checklist component

--- web/src/routes/_authenticated/aircraft/$aircraftId/learn.tsx
+++ web/src/routes/_authenticated/aircraft/$aircraftId/learn.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, Checkbox, List, ListItem, ListItemIcon, ListItemText, Stack, Typography } from '@mui/material';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { useSuspenseQuery } from '@tanstack/react-query';
@@ -48,10 +48,32 @@ function LearnMode() {
         </Stack>
       </Stack>
 
-      {/* Flight deck viewer. TODO: add left column before this */}
-      <Box className="max-w-2/3 py-8">
-        <FlightDeckViewer views={aircraft.views} />
-      </Box>
+      <Stack direction="row" gap={3} className="py-8">
+        <Stack>
+          <Typography variant="h5" className="pt-0">Tasks</Typography>
+          <List disablePadding className="w-70">
+            <ListItem disablePadding>
+              <ListItemIcon><Checkbox checked={true} disableRipple color="success" /></ListItemIcon>
+              <ListItemText primary="Control lock - REMOVE" />
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemIcon><Checkbox checked={true} disableRipple color="success" /></ListItemIcon>
+              <ListItemText primary="Avionics - OFF" />
+            </ListItem>
+            <ListItem disablePadding className="border-2 border-gray-400">
+              <ListItemIcon><Checkbox checked={false} disableRipple color="success" /></ListItemIcon>
+              <ListItemText primary="Master switch battery - ON" />
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemIcon><Checkbox checked={false} disableRipple color="success" /></ListItemIcon>
+              <ListItemText primary="Fuel guages - CHECK" />
+            </ListItem>
+          </List>
+        </Stack>
+        <Box className="max-w-2/3">
+          <FlightDeckViewer views={aircraft.views} />
+        </Box>
+      </Stack>
     </>
   )
 }
