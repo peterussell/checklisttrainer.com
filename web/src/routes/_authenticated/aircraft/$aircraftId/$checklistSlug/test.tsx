4c5aa39229a3603efe0c4d94fa2a04b74d587ab8
Pete Russell <pinkbatz@gmail.com>
2025-10-03 22:00:17 -0500

    Test mode page scaffolding

--- web/src/routes/_authenticated/aircraft/$aircraftId/$checklistSlug/test.tsx
+++ web/src/routes/_authenticated/aircraft/$aircraftId/$checklistSlug/test.tsx
@@ -1,5 +1,12 @@
-import { Typography } from '@mui/material';
-import { createFileRoute } from '@tanstack/react-router'
+import { Alert, Stack, Typography } from '@mui/material';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { createFileRoute, Link } from '@tanstack/react-router'
+import type { Aircraft } from '../../../../../../../core/models/Aircraft';
+import { aircraftDetailQuery } from '../../../../../queries/aircraftDetailQuery';
+import type { Checklist } from '../../../../../../../core/models/Checklist';
+import { LeftSidebarLayout } from '../../../../../shared/layout/LeftSidebarLayout';
+import InfoOutline from '@mui/icons-material/InfoOutline';
+import { PageHeader } from '../../../../../shared/components/PageHeader';
 
 export const Route = createFileRoute(
   '/_authenticated/aircraft/$aircraftId/$checklistSlug/test',
@@ -8,12 +15,50 @@ export const Route = createFileRoute(
 })
 
 function TestMode() {
-  const { aircraftId } = Route.useParams();
+  const { aircraftId, checklistSlug } = Route.useParams();
 
-  // tmp
-  console.log(aircraftId);
+  const { data } = useSuspenseQuery<Aircraft | null>({
+    queryKey: ['aircraft', aircraftId],
+    queryFn: () => aircraftDetailQuery(aircraftId),
+  });
+
+    // Also handled at parent, but repeat here to be safe
+  if (!data) return <Typography>Failed to load aircraft</Typography>
+
+  const aircraft: Aircraft = data;
+  const checklist: Checklist | undefined = aircraft.checklists.find((c: Checklist) => c.slug === checklistSlug);
 
   return (
-    <Typography>{aircraftId} - Test mode</Typography>
-  )
+    <>
+      <Stack gap={2}>
+        <PageHeader
+          title={[aircraft.registration, "Practice mode"]}
+          subtitle={aircraft.description}
+          backLink={<Link to="/aircraft/$aircraftId" params={{ aircraftId }}>Back to Checklists</Link>}
+        />
+
+        {/* Instructions */}
+        <Alert icon={<InfoOutline fontSize="large" />} severity="info" className="items-center">
+          <Typography>
+              <strong>Test mode</strong> provides immediate feedback as you complete each step of the checklist
+              using the interactive flight deck. If the selected task is correct, it will be revealed under <strong>Tasks</strong>.
+              Stuck? Click <strong>Hint</strong> to show the next checklist item.
+            </Typography>
+        </Alert>
+
+        <LeftSidebarLayout
+          sidebarContent={
+            <>
+              <Typography>Left sidebar</Typography>
+            </>
+          }
+          mainContent={
+            <>
+              <Typography>Main content</Typography>
+            </>
+          }
+        />
+      </Stack>
+    </>
+  );
 }
